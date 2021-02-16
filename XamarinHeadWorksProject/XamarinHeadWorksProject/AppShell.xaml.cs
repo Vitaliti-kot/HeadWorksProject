a94b949a050dbb2b564f875376d031353b5358e5
Vitalii Kovalkov <step7shop@gmail.com>
2021-02-17 00:11:01 +0200

    Creating project

---
+++ XamarinHeadWorksProject/XamarinHeadWorksProject/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinHeadWorksProject.ViewModels;
+using XamarinHeadWorksProject.Views;
+using Xamarin.Forms;
+
+namespace XamarinHeadWorksProject
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
